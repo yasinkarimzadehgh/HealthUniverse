46c8f872c280bd333cfab38a3d019ef5e1c74ef4
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2025-03-16 19:54:16 +0330

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
-import { Provider } from "react-redux";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import store from "./redux/store";
 import './styles/main.css'
 
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
 )
