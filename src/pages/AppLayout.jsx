70c5289dfa0c1372e297e4d3cc8d17db1672a45b
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2025-03-19 17:49:01 +0330

    first commit

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,18 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import '../styles/AppLayout.css'
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
+
+import "../styles/AppLayout.css";
 
 function AppLayout() {
     return (
         <div className="app-layout">
             <Header />
+            <ScrollToTop />
             <main className="main">
                 <Outlet />
             </main>
             <Footer />
-
-
         </div>
     );
 }
